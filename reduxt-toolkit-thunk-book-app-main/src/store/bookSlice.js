7c249b6e64b5e4a6238871ff0f1706e9057d7060
Aymen Mosrati <aymenmosrati6@gmail.com>
2022-11-30 00:40:49 +0100

    book store with json-server

---
+++ reduxt-toolkit-thunk-book-app-main/src/store/bookSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { books: null };
+
+const bookSlice = createSlice({
+    name: "book",
+    initialState,
+    reducers: {}
+})
+
+export default bookSlice.reducer;
