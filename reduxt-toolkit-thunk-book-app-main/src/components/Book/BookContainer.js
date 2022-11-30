aeba6be05a1e12731fefb0be00bf1e64283019df
Taki Academy <takiacademy@Takis-Mac-mini.local>
2022-11-30 15:15:31 +0100

    create loadind and fetch api

--- reduxt-toolkit-thunk-book-app-main/src/components/Book/BookContainer.js
+++ reduxt-toolkit-thunk-book-app-main/src/components/Book/BookContainer.js
@@ -1,24 +1,27 @@
-import React, { Fragment, useEffect } from 'react';
-import BookInfo from './BookInfo';
-import BooksList from './BooksList';
-import { useDispatch } from 'react-redux'
+import React, { Fragment, useEffect } from "react";
+import BookInfo from "./BookInfo";
+import BooksList from "./BooksList";
+import { useDispatch, useSelector } from "react-redux";
 
-import './book.css';
-import { getBooks } from '../../store/bookSlice';
+import "./book.css";
+import { getBooks } from "../../store/bookSlice";
 
 const PostContainer = () => {
+  const { isLoading, books } = useSelector((state) => state.books);
   const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(getBooks());
+    dispatch(getBooks({ id: 1 }));
   }, [dispatch]);
+
   return (
     <Fragment>
-      <hr className='my-5' />
-      <div className='row'>
-        <div className='col'>
-          <BooksList />
+      <hr className="my-5" />
+      <div className="row">
+        <div className="col">
+          <BooksList isLoading={isLoading} books={books} />
         </div>
-        <div className='col side-line'>
+        <div className="col side-line">
           <BookInfo />
         </div>
       </div>
--- reduxt-toolkit-thunk-book-app-main/src/store/bookSlice.js
+++ reduxt-toolkit-thunk-book-app-main/src/store/bookSlice.js
@@ -1,41 +1,58 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const initialState = { books: null };
+const initialState = { books: null, isLoading: false };
 
-// fire = techta8al 
+// fire = techta8al
 // this function we return three types of functions:
 //  penndind = time of called data
-// fulfilled = return data of success 
+// fulfilled = return data of success
 // rejected = return error
-// this function because to cosume ani Query (API or GraphQL) and return async function 
-export const getBooks = createAsyncThunk('book/getBooks', async (args, thunkAPI) => {
+// this function because to cosume ani Query (API or GraphQL) and return async function
+export const getBooks = createAsyncThunk(
+  "book/getBooks",
+  //   args : if you send data in parametres of deipatch we store this data in this parameters (args, can be rename any other name)
+  async (args, thunkAPI) => {
     try {
-        const res = await fetch('http://localhost:4000/books');
-        const data = await res.json();
-        return data;
+      // part 2
+      //dispatch({type:'book/getBooks/pending', payload: undefined})
+      const res = await fetch("http://localhost:4000/books");
+      const data = await res.json();
+      return data;
+      //dispatch({type:'book/getBooks/fulfilled', payload: data})
     } catch (error) {
-        console.log(error);
+      console.log(error);
+      //dispatch({type:'book/getBooks/rejected', payload: error})
     }
-})
+  }
+);
 
+// part1 getbooks: {pending, fulfilled, rejected}
+// getbooks -> createAsyncThunk ->create 3 type of actions
+// pending createAction('book/getBooks/pending', (payload) =>{return payload})
+// fulfilled createAction('book/getBooks/pending', (payload) =>{return payload})
+// rejected createAction('book/getBooks/pending', (payload) =>{return payload})
 
 const bookSlice = createSlice({
-    name: "book",
-    initialState,
-    //reducers: pure , no accepted the aside function 
-    reducers: {},
-    // accepted the acide function and asid effect API or GraphQL .....
-    extraReducers:{
-        [getBooks.pending]:(state,action)=>{
-            console.log(action);
-        },
-        [getBooks.fulfilled]:(state,action)=>{
-            console.log(action);
-        },
-        [getBooks.rejected]:(state,action)=>{
-            console.log(action);
-        },
-    }
-})
+  name: "book",
+  initialState,
+  //reducers: pure , no accepted the aside function
+  reducers: {},
+  // accepted the acide function and asid effect API or GraphQL .....
+  extraReducers: {
+    [getBooks.pending]: (state, action) => {
+      state.isLoading = true;
+      //   console.log(action);
+    },
+    [getBooks.fulfilled]: (state, action) => {
+      //   console.log(action);
+      state.isLoading = false;
+      state.books = action.payload;
+    },
+    [getBooks.rejected]: (state, action) => {
+      //   console.log(action);
+      state.isLoading = false;
+    },
+  },
+});
 
 export default bookSlice.reducer;
