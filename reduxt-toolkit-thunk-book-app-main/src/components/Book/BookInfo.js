c39e1ec46f8b5281a929b40de3a4c59927d08ff7
Taki Academy <takiacademy@Takis-Mac-mini.local>
2022-12-02 15:38:17 +0100

    haw to map object with values or with object , it's the same

--- reduxt-toolkit-thunk-book-app-main/src/components/Book/BookInfo.js
+++ reduxt-toolkit-thunk-book-app-main/src/components/Book/BookInfo.js
@@ -5,6 +5,7 @@ const BookInfo = ({ info }) => {
   return (
     <Fragment>
       <h2>Book Details</h2>
+      {/* {Object.values(info).length > 0 ? ( */}
       {Object.keys(info).length > 0 ? (
         <div>
           <p className="fw-bold">Title:{info.title}</p>
